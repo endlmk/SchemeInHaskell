993b4b84125448b94ca29550abf8f5dbe299a6a4
endlmk <1269467+endlmk@users.noreply.github.com>
2023-05-08 23:28:57 +0900

    Parse string.

--- src/Parser.hs
+++ src/Parser.hs
@@ -3,18 +3,20 @@
 
 module Parser
   ( parseExpression,
-    LispVal (Atom, Number),
+    LispVal (Atom, Number, String),
   )
 where
 
 import Data.Text (pack)
 import Data.Text qualified as T
+import GHC.Float (fromRat'')
 import Text.Parsec
 import Text.Parsec.Text
 
 data LispVal
   = Atom T.Text
   | Number Integer
+  | String T.Text
   deriving (Eq, Show)
 
 atom :: Parser LispVal
@@ -33,5 +35,12 @@ negNumber = do
   num <- many1 digit
   return (Number ((read num :: Integer) * (-1)))
 
+parseString :: Parser LispVal
+parseString = do
+  _ <- char '\"'
+  str <- many (noneOf "\"")
+  _ <- char '\"'
+  return (String (T.pack str))
+
 parseExpression :: String -> Either ParseError LispVal
-parseExpression s = parse (atom <|> number <|> negNumber) "error" (pack s)
+parseExpression s = parse (atom <|> number <|> negNumber <|> parseString) "error" (pack s)
--- test/ParserSpec.hs
+++ test/ParserSpec.hs
@@ -16,3 +16,5 @@ spec = do
       parseExpression "123" `shouldBe` Right (Number 123)
     it "parses negative number" $ do
       parseExpression "-2187" `shouldBe` Right (Number (-2187))
+    it "parses string" $ do
+      parseExpression "\"Test\"" `shouldBe` Right (String "Test")
